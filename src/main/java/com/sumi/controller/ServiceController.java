cdb447ae0e40a55a05a482a720a6c401c5ec2f15
VikasN <vikas.n649@gmail.com>
2016-07-20 00:42:25 -0700

    Creating Service Controller and Author Class

--- src/main/java/com/sumi/controller/ServiceController.java
+++ src/main/java/com/sumi/controller/ServiceController.java
@@ -1,6 +1,12 @@
 package com.sumi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/helpme")
 public class ServiceController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ServiceController.class);
+
+    @Value("${author.name}") String authorName;
+
+    @RequestMapping(value = "author", method = {RequestMethod.GET, RequestMethod.POST},
+    produces = "application/json")
+    public ResponseEntity<Author> getAuthorInfo(@Value("${author.name}") String authorName,
+                                                @Value("${author.email}") String authorEmail){
+        final Author author = new Author();
+        author.setName(authorName);
+        author.setEmail(authorEmail);
+        return new ResponseEntity<>(author, HttpStatus.OK);
+    }
+
 
 }
